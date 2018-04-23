57aea12da27da9073aa636c89515749ccd30fefe
Richie Bendall <richiebendall@gmail.com>
2018-04-23 13:13:53 +1200

    Added Windows Installer And Pack Data

---
+++ Install Texture Pack.bat
@@ -0,0 +1,48 @@
+@echo off
+title Install Rosources Texture Pack
+cls
+
+echo Welcome to the Rosources texture pack installer
+echo Press any key to begin installation...
+pause>nul
+echo.
+if exist "Rosources Texture Pack*" goto packexist else goto packnoexist
+
+:packnoexist
+echo Unable to find Rosources Texture Pack folder
+echo Please ensure this folder exists before installing
+echo.
+goto finish
+
+:packexist
+echo Rosources Texture Pack Folder Found!
+if exist %appdata%\.minecraft\resourcepacks goto folderexists else goto foldernotexists
+
+:folderexists
+echo Minecraft Resource Packs Folder Found!
+echo.
+if exist "%appdata%\.minecraft\resourcepacks\Rosources Texture Pack"  goto removeoldversion
+echo Installing...
+goto installfolder
+
+:installfolder
+xcopy /I "Rosources Texture Pack" "%appdata%\.minecraft\resourcepacks\Rosources Texture Pack" /V /E /H /K>nul
+echo.
+echo Finished installing Rosources texture pack
+goto finish
+
+:foldernotexists
+echo Unable to find Minecraft Resource Packs Folder!
+echo Please ensure that the directory "%appdata%\.minecraft\resourcepacks" exists
+goto finish
+
+:removeoldversion
+echo Removing old version...
+rmdir /S /Q "%appdata%\.minecraft\resourcepacks\Rosources Texture Pack"
+echo Installing new version...
+goto installfolder
+
+:finish
+echo Press any key to close the installer...
+pause>nul
+exit 0
